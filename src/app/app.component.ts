bcd97961acd4aed784ef1d9eb2cf92fe39e75256
Milan <mistikone1@gmail.com>
2020-02-08 18:35:12 +0100

    Angular Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,11 +7,38 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f', {static: true}) signupForm: NgForm
+  defaultQuestion: string = 'pet';
+  answer: string = '';
+  genders = ['male', 'female'];
+
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
-  onSubmit(form: NgForm){
-    console.log(form);
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  //   this.user.username = this.signupForm.value.
+  // }
+
+  onSubmit() {
+    console.log(this.signupForm);
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+    this.submitted = true;
+
+    this.signupForm.reset();
   }
 }
